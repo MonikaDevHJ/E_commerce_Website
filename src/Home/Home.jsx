7c90b1960102b2589853a831c20e5d2822c3392f
Monika H J <monikahj7@gmail.com>
2024-07-18 15:05:59 +0530

    Home Catagory Is Done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import HomeCatagory from './HomeCatagory'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <HomeCatagory/>
     </div>
   )
 }
