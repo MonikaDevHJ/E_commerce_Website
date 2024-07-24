18ac3ef4ac0ff0893094ea4e9a9c0f3c877d44b7
Monika H J <monikahj7@gmail.com>
2024-07-24 12:04:05 +0530

    Home Page is Completed UI

--- src/Home/LocationSpread.jsx
+++ src/Home/LocationSpread.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const title = "More Then 60,000 Customers";
-
 
 const desc =
   "Buy products on your any device with our app & enjoy your time what you want. Just download & install & start to shopping";
@@ -47,39 +45,33 @@ const clientsList = [
 ];
 
 const LocationSpread = () => {
-  return <div className="client-section style-2 padding-tb">
+  return (
+    <div className="clients-section style-2 padding-tb">
       <div className="container">
         <div className="section-header text-center">
-            <h2 className="titile"> {title}
-
-            </h2>
-            <p>{desc}</p>
-
-
+          <h2 className="titile">{title}</h2>
+          <p>{desc}</p>
         </div>
         {/* main content */}
         <div className="section-wrapper">
-            <div className="client">
-                {
-                    clientsList.map((val,i)=>{
-                        <div key={i} className="client-list">
-                            <Link to="/sign-up" className="client-content">
-                            <span>{val.text}</span>
-                            </Link>
-                            <div className="client-thumb">
-                                   <img src={val.imgUrl} alt="" />
-                            </div>
-                        </div>
-
-                    })
-                }
-
-            </div>
-
+          <div className="clients">
+            {clientsList.map((val, i) => {
+              return (
+                <div key={i} className="client-list">
+                  <Link to="/sign-up" className="client-content">
+                    <span>{val.text}</span>
+                  </Link>
+                  <div className="client-thumb">
+                    <img src={val.imgUrl} alt={val.imgAlt} />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default LocationSpread;
