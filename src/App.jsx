b75d644a9611e2cea0867542581015a096370a47
Monika H J <monikahj7@gmail.com>
2024-07-24 13:33:09 +0530

    Footer Page is Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import NavItems from "./components/Navitems.jsx"
 import Blog from "./blog/Blog.jsx"
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
     <NavItems/>
     <Outlet/>
+    <Footer/>
     </>
   )
 } 
